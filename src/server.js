f351995a86922407029ebf1030cacc175f7ffa2f
Dustin Byrne <dbyrne@conjur.net>
2016-06-01 19:29:08 -0400

    now responding with which vars exist

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
 
+var verify = function(varName) {
+  return typeof process.env[varName] === 'undefined' ?
+    varName + ' does not exist in the environment!\n' : 
+    varName + ' exists in the environment!\n';
+}
+
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send(verify('API_KEY')     +
+           verify('PRIVATE_KEY') +
+           verify('DATABASE_PASSWORD'));
 });
 
 app.listen(process.env.PORT || 8080);
